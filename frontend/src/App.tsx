58183632c0ccfd9cb0346ba50c9a2440b0916486
Jan Czajka <czajka.jan12@gmail.com>
2025-02-09 14:25:31 +0100

    feat(MED-6): husky test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ export const App = () => {
       <div className="flex h-screen w-screen items-center justify-center bg-gray-300">
         <Paper>
           
+          
           <Button>recepta na medyczną marihuane kliknij tutaj!!!</Button>
         </Paper>
       </div>
