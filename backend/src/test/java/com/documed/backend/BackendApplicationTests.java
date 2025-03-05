932ff762b3a6f6c6468bb7117f07ecc22e828303
Jan Czajka <czajka.jan12@gmail.com>
2025-03-05 20:37:42 +0100

    feat(MED-37): mock database

--- backend/src/test/java/com/documed/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/documed/backend/BackendApplicationTests.java
@@ -1,10 +1,20 @@
 package com.documed.backend;
 
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class BackendApplicationTests {
+
+  @Mock private DataSource dataSource;
+
+  @InjectMocks private BackendApplicationTests backendApplicationTests;
 
   @Test
   void contextLoads() {}
