ef9f2f32e4f2c95cbd7e376db2f9c5f05b45e378
Jan Czajka <czajka.jan12@gmail.com>
2025-03-05 20:41:24 +0100

    feat(MED-37): test mock improvement

--- backend/src/test/java/com/documed/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/documed/backend/BackendApplicationTests.java
@@ -1,20 +1,13 @@
 package com.documed.backend;
 
-import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+      "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+    })
 class BackendApplicationTests {
-
-  @Mock private DataSource dataSource;
-
-  @InjectMocks private BackendApplicationTests backendApplicationTests;
 
   @Test
   void contextLoads() {}
