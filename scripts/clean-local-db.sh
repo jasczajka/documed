42e2e800eebee033ce2c60c883afe21abadca887
Jan Czajka <czajka.jan12@gmail.com>
2025-04-21 16:26:54 +0200

    feat(NOTICKET): add scripts to init local database

---
+++ scripts/clean-local-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONTAINER_NAME="local-postgres"
+VOLUME_NAME="pgdata"
+
+echo "🗑 Removing container '$CONTAINER_NAME' (if exists)..."
+docker rm -f $CONTAINER_NAME 2>/dev/null || echo "No container to remove."
+
+echo "🧹 Removing volume '$VOLUME_NAME'..."
+docker volume rm $VOLUME_NAME 2>/dev/null || echo "No volume to remove."
