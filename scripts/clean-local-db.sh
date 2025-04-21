89feb46cc124cdd2e67d7374df83c1b4bbd368be
Jan Czajka <czajka.jan12@gmail.com>
2025-04-21 16:29:14 +0200

    feat(NOTICKET): cosmetics

--- scripts/clean-local-db.sh
+++ scripts/clean-local-db.sh
@@ -3,8 +3,8 @@
 CONTAINER_NAME="local-postgres"
 VOLUME_NAME="pgdata"
 
-echo "🗑 Removing container '$CONTAINER_NAME' (if exists)..."
+echo "Removing container '$CONTAINER_NAME' (if exists)..."
 docker rm -f $CONTAINER_NAME 2>/dev/null || echo "No container to remove."
 
-echo "🧹 Removing volume '$VOLUME_NAME'..."
+echo "Removing volume '$VOLUME_NAME'..."
 docker volume rm $VOLUME_NAME 2>/dev/null || echo "No volume to remove."
