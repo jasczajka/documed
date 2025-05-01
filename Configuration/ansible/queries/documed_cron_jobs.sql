5c253a95a8ab432cd78f28b15ded43aad63ca9b0
Jan Czajka <czajka.jan12@gmail.com>
2025-05-01 13:02:23 +0200

    feat(MED-111): separate cron schedule scripts for local db and remote db

--- Configuration/ansible/queries/documed_cron_jobs.sql
+++ Configuration/ansible/queries/documed_cron_jobs.sql
@@ -1,10 +1,13 @@
 
+
+-- Schedule OTP cleanup to run daily at midnight (00:00)
 SELECT cron.schedule(
   'cleanup_pending_users_daily',
   '0 0 * * *',
   $$SELECT cleanup_pending_users();$$
 );
 
+-- Schedule pending users cleanup to run daily at midnight (00:00)
 SELECT cron.schedule(
   'cleanup_opts_daily',
   '0 0 * * *',
