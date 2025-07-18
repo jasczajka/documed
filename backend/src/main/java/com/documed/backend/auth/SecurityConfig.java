bf64e877f9b6ca69657ed62ee0c384f7785630b4
Jan Czajka <czajka.jan12@gmail.com>
2025-07-19 00:41:14 +0200

    feat(MED-151): adjust access check

--- backend/src/main/java/com/documed/backend/auth/SecurityConfig.java
+++ backend/src/main/java/com/documed/backend/auth/SecurityConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
@@ -82,10 +82,14 @@ public class SecurityConfig {
                   .permitAll()
                   .requestMatchers("/api/medicines/import")
                   .access(
-                      new WebExpressionAuthorizationManager(
-                          "@securityConfig.checkIp(request, '"
-                              + dbServerIp
-                              + "') or hasRole('ADMINISTRATOR')"));
+                      (authentication, context) -> {
+                        HttpServletRequest request = context.getRequest();
+                        boolean isAllowed =
+                            checkIp(request, dbServerIp)
+                                || authentication.get().getAuthorities().stream()
+                                    .anyMatch(a -> a.getAuthority().equals("ROLE_ADMINISTRATOR"));
+                        return new AuthorizationDecision(isAllowed);
+                      });
               if (env.acceptsProfiles("local")) {
                 auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**")
                     .permitAll();
