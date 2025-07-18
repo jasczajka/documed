b95baaef93806a1526f740d1ebb5199e14a78eff
Jan Czajka <czajka.jan12@gmail.com>
2025-07-18 23:35:52 +0200

    feat(MED-151): version two of allowing ansible to trigger medicine import

--- backend/src/main/java/com/documed/backend/auth/SecurityConfig.java
+++ backend/src/main/java/com/documed/backend/auth/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.cors.CorsConfiguration;
@@ -28,6 +29,9 @@ public class SecurityConfig {
 
   @Value("${allowed_origins}")
   private String[] allowedOrigins;
+
+  @Value("${spring.datasource.ip}")
+  private String dbServerIp;
 
   public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, Environment env) {
     this.jwtAuthFilter = jwtAuthFilter;
@@ -65,7 +69,11 @@ public class SecurityConfig {
                   .requestMatchers("/api/facilities")
                   .permitAll()
                   .requestMatchers("/actuator/health")
-                  .permitAll();
+                  .permitAll()
+                  .requestMatchers("/api/medicines/import")
+                  .access(
+                      new WebExpressionAuthorizationManager(
+                          "hasIpAddress('" + dbServerIp + "') or hasRole('ADMINISTRATOR')"));
 
               if (env.acceptsProfiles("local")) {
                 auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**")
